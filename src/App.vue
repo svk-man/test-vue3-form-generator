d8bad390c372d8a025f07429414c953e7591570f
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-08-16 18:12:51 +0300

    feat: add json form template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <FormJsonEditor />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import FormJsonEditor from './components/FormJsonEditor.vue';
 
-export default defineComponent({
+defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    FormJsonEditor,
   },
 });
 </script>
@@ -20,8 +19,9 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/FormJsonEditor.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="form-json">
+    <label for="form-json" class="form-json__label">Введите JSON-представление формы:
+      <textarea id="form-json" class="form-json__textarea"
+        cols="30" rows="10" v-model="formJson"></textarea>
+    </label>
+    <button type="submit" class="form-json__button">Сгенерировать форму</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+defineComponent({
+  name: 'FormJsonEditor',
+});
+
+const formJson = ref('');
+</script>
+
+<style scoped lang="scss">
+.form-json {
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  &__textarea {
+    width: 100%;
+    margin-top: 10px;
+  }
+}
+</style>
